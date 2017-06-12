c753336ef6087941105306493561bb140ee26128
simone <simone.zippo@gmail.com>
2017-06-12 14:58:20 +0200

    corrected catalogue

--- src/main/java/repository/CatalogueRepository.java
+++ src/main/java/repository/CatalogueRepository.java
@@ -1,12 +1,12 @@
 package repository;
 
-import model.Batch;
 import model.Catalogue;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
  * Created by simone on 09/06/17.
  */
-public interface CatalogueRepository  extends MongoRepository<Catalogue, Long> {
+public interface CatalogueRepository extends MongoRepository<Catalogue, Long> {
 
+    Catalogue findById(String id);
 }
